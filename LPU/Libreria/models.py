de2ed7c279f2c4c487164feccb7929bb9c8bc515
Stressed-Cat <ze.aqua.doggo@gmail.com>
2022-06-05 21:29:01 -0400

    Se hacen los models de cada app, sin agregar foreign keys aun

--- LPU/Libreria/models.py
+++ LPU/Libreria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class articulo(models.Model):
+    nombre = models.CharField(max_length=125)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+
+class genero(models.Model):
+    nombre = models.CharField(max_length=125)
+
+class editorial(models.Model):
+    nombre = models.CharField(max_length=125)
+
+class tipo(models.Model):
+    nombre = models.CharField(max_length=125)
+
+class idioma(models.Model):
+    nombre = models.CharField(max_length=125)
--- LPU/Ventas/models.py
+++ LPU/Ventas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#class ejemplar(models.Model):
+    # Se meteria la foreign key creo, despues lo hago a excepción que lo hagas tu primero
+
+class compra(models.Model):
+    precio_total = models.IntegerField()
+    precio_total = models.DateField()
